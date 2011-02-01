d543d3d5a9f7f0f3820d2046a720e1e106804480
Doug Reeves <guodman@gmail.com>
2011-02-01 17:07:13 -0600

    consistent code formatting.

--- src/org/guodman/groceryAssistant/Main.scala
+++ src/org/guodman/groceryAssistant/Main.scala
@@ -12,34 +12,34 @@ import android.widget.LinearLayout
 import android.widget.Button
 
 class Main extends Activity {
-  override def onCreate(savedInstanceState: Bundle): Unit = {
-    super.onCreate(savedInstanceState)
-    var menu: LinearLayout = new LinearLayout(this)
-    menu.setOrientation(LinearLayout.VERTICAL)
-    setContentView(menu)
-    var grocList: Button = new Button(this)
-    grocList.setText("Grocery List")
-    grocList.setOnClickListener(new ViewSwitcher(this, classOf[GroceryList]))
-    menu.addView(grocList)
-    var itemList: Button = new Button(this)
-    itemList.setText("Item List")
-    itemList.setOnClickListener(new ViewSwitcher(this, classOf[FoodList]))
-    menu.addView(itemList)
-    var createItem: Button = new Button(this)
-    createItem.setText("Create Item")
-    createItem.setOnClickListener(new ViewSwitcher(this, classOf[CreateItem]))
-    menu.addView(createItem)
-  }
+	override def onCreate(savedInstanceState: Bundle): Unit = {
+		super.onCreate(savedInstanceState)
+		var menu: LinearLayout = new LinearLayout(this)
+		menu.setOrientation(LinearLayout.VERTICAL)
+		setContentView(menu)
+		var grocList: Button = new Button(this)
+		grocList.setText("Grocery List")
+		grocList.setOnClickListener(new ViewSwitcher(this, classOf[GroceryList]))
+		menu.addView(grocList)
+		var itemList: Button = new Button(this)
+		itemList.setText("Item List")
+		itemList.setOnClickListener(new ViewSwitcher(this, classOf[FoodList]))
+		menu.addView(itemList)
+		var createItem: Button = new Button(this)
+		createItem.setText("Create Item")
+		createItem.setOnClickListener(new ViewSwitcher(this, classOf[CreateItem]))
+		menu.addView(createItem)
+	}
 
-  override def onCreateOptionsMenu(menu: Menu): Boolean = {
-    var inflater = getMenuInflater()
-    inflater.inflate(R.menu.mainmenu, menu)
-    return true
-  }
+	override def onCreateOptionsMenu(menu: Menu): Boolean = {
+		var inflater = getMenuInflater()
+		inflater.inflate(R.menu.mainmenu, menu)
+		return true
+	}
 }
 
 class ViewSwitcher (context : Context, cls : Class[_]) extends OnClickListener {
-  override def onClick(v: View): Unit = {
-    context.startActivity(new Intent(context, cls))
-  }
+	override def onClick(v: View): Unit = {
+		context.startActivity(new Intent(context, cls))
+	}
 }
