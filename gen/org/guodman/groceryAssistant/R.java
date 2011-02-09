b4407421c8391e198c7fd050123f2ebb088ec0fe
Doug Reeves <guodman@gmail.com>
2011-02-08 20:11:45 -0600

    removed the inactive menu from the main screen

--- gen/org/guodman/groceryAssistant/R.java
+++ gen/org/guodman/groceryAssistant/R.java
@@ -18,8 +18,6 @@ public final class R {
         public static final int addtolist=0x7f060002;
         public static final int clearchecked=0x7f060004;
         public static final int edit=0x7f060003;
-        public static final int newentry=0x7f060005;
-        public static final int quit=0x7f060006;
         public static final int tag=0x7f060000;
     }
     public static final class layout {
@@ -29,7 +27,6 @@ public final class R {
     public static final class menu {
         public static final int foodmenu=0x7f050000;
         public static final int grocerymenu=0x7f050001;
-        public static final int mainmenu=0x7f050002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/org/guodman/groceryAssistant/Main.scala
+++ src/org/guodman/groceryAssistant/Main.scala
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.view.View
 import android.view.View.OnClickListener
-import android.view.MenuInflater
-import android.view.Menu
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout
@@ -31,12 +29,6 @@ class Main extends Activity {
 		createItem.setText("Create Item")
 		createItem.setOnClickListener(new ViewSwitcher(this, classOf[CreateItem]))
 		menu.addView(createItem)
-	}
-
-	override def onCreateOptionsMenu(menu: Menu): Boolean = {
-		var inflater = getMenuInflater()
-		inflater.inflate(R.menu.mainmenu, menu)
-		return true
 	}
 }
 
